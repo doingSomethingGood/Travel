f759999aa8b344eaaf22c0cd2f4d125ddd49c6fd
doingSomethingGood <978217648@qq.com>
2019-03-22 15:46:27 +0800

    主页面

---
+++ src/assets/pages/home/components/Icons.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page,index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" alt="">
+            <!--<img class="icon-img-content" src="../../../jing_dian_6.png" alt="">-->
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "icons",
+    data() {
+      return {
+        iconList: [
+          {
+            id: '0001',
+            imgUrl: '../../../jing_dian.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0002',
+            imgUrl: '../../../jing_dian_1.png',
+            desc: '动物园'
+          },
+          {
+            id: '0003',
+            imgUrl: '../../../jing_dian_2.png',
+            desc: '植物园'
+          },
+          {
+            id: '0004',
+            imgUrl: '../../../jing_dian_3.png',
+            desc: '啊哈哈乐园'
+          },
+          {
+            id: '0005',
+            imgUrl: '../../../jing_dian_4.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0007',
+            imgUrl: '../../../jing_dian_6.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0008',
+            imgUrl: '../../../jing_dian_7.png',
+            desc: '景点门票'
+          },
+          {
+            id: '0009',
+            imgUrl: '../../../jing_dian_8.png',
+            desc: '景点门票'
+          },
+
+        ]
+      }
+    },
+    computed: {
+      pages() {
+        const pages = [];
+        this.iconList.forEach((item, index) => {
+          const page = Math.floor(index / 8);
+          if (!pages[page]) {
+            pages[page] = [];
+          }
+          pages[page].push(item);
+        });
+        return pages;
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  @import "~styles/varibles.styl"
+  @import "~styles/mixins.styl"
+  .icons >>> .swiper-container
+    height 0
+    padding-bottom 50%
+  .icons
+    margin-top .1rem
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        left 0
+        right 0
+        top 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        text-align center
+        color $darkTextColor
+        ellipsis()
+</style>
---
+++ src/assets/pages/home/components/Weekend.vue
@@ -0,0 +1,113 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" alt="">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "HomeWeekend",
+    data(){
+      return{
+        recommendList:[
+
+          {
+            id:'0001',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0002',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0003',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0004',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0005',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0006',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0007',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0008',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          },
+          {
+            id:'0009',
+            imgUrl:'../../../jing_dian.png',
+            title:'美国哈佛开发就不浪费快递费就好了',
+            desc:'临床技能法律床律层面'
+          }
+
+
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+
+  @import "~styles/mixins.styl"
+
+  .title
+    margin-top .2rem
+    line-height .8rem
+    background #eee
+    text-indent .2rem
+  .item-img-wrapper
+    overflow hidden
+    padding-bottom 33.9%
+    height 0
+  .item-img
+    width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+</style>
